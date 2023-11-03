6b4278f8f6cf586850707b0b10eb485cd8a3203a
Rafael <rafael-rafa2.1@hotmail.com>
2023-11-03 20:24:29 -0300

    Add new image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <div className='h-screen w-screen max-h-full flex items-center justify-center p-5'>
       <a
         href='https://www.instagram.com/grupodeuseamor'
-        className='h-[40rem] w-[40rem] max-h-full max-w-full flex items-center justify-center rounded-xl  shadow-2xl hover:scale-90 transition-all'
+        className='h-[40rem] w-[35rem] max-h-full max-w-full flex items-center justify-center rounded-xl  shadow-2xl hover:scale-90 transition-all'
       >
         <Image
-          src='/save-the-date.jpeg'
+          src='/image.png'
           alt='save-the-date 30/11'
-          width={640}
-          height={640}
+          width={950}
+          height={1280}
           className='rounded-xl object-contain border-4 border-gray-100'
         />
       </a>
